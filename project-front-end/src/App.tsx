9efb8102355b708e62236a2df4e211a30e1661f2
Juliana Tissiani Diorio <julianatdiorio@gmail.com>
2022-11-01 18:30:05 -0300

    feat(landing page) create landing page

--- project-front-end/src/App.tsx
+++ project-front-end/src/App.tsx
@@ -1,5 +1,9 @@
+import { LandingPage } from "./features/pages/landingPage";
+
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <LandingPage />
+  </div>;
 }
 
 export default App;
--- project-front-end/src/features/pages/landingPage/index.tsx
+++ project-front-end/src/features/pages/landingPage/index.tsx
@@ -1,3 +1,19 @@
+import { BgStyled, DivInfos, LogoStyled, PLandingStyled, TitleLandingStyled, DivButtonsLandingStyled, ButtonLandingStyled } from './styles';
+
 export const LandingPage = () => {
-  return <div></div>;
+  return (
+    <BgStyled>
+      <LogoStyled />
+      <DivInfos>
+        <TitleLandingStyled>Você está precisando realizar serviços de manutenção em casa?</TitleLandingStyled>
+        <PLandingStyled>Entre já no nosso site e resolva todos os seus problemas! </PLandingStyled>
+        <DivButtonsLandingStyled>
+          <ButtonLandingStyled>Quero conhecer</ButtonLandingStyled>
+          <ButtonLandingStyled>Fazer login</ButtonLandingStyled>
+        </DivButtonsLandingStyled>
+      </DivInfos>
+    </BgStyled>
+  )
+    
+
 };

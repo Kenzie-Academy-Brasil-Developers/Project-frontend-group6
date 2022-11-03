7ab76118573a74ffaaea6cbd1859006e2a18d72f
Juan Sgarbi <juan.sgarbi1@hotmail.com>
2022-11-03 16:50:21 -0300

    feat: add feature perfil

--- project-front-end/src/features/pages/profile/index.tsx
+++ project-front-end/src/features/pages/profile/index.tsx
@@ -2,13 +2,20 @@ import { CProfile } from "../../layouts/CProfile";
 import { HireProfile } from "../../layouts/HireProfile";
 import { Header } from "../../layouts/header";
 import { Avatar } from "@mui/material";
+import { Container } from "../../styles/container";
+import { H2Styled } from "./styles";
+import { UserContext } from "../../../context/UserContext";
 
-export const Profile = () => {
+export const Profile = ({ user }: any) => {
   return (
     <>
       <Header>
         <Avatar src="https://cdn-icons-png.flaticon.com/512/219/219969.png" />
       </Header>
+      <Container>
+        <H2Styled>Editar Perfil</H2Styled>
+        {user.is_hire ? <HireProfile /> : <CProfile />}
+      </Container>
     </>
   );
 };
